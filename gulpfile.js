e366b9a83602d32be83272f721273137673e7323
Алексей <agragregra@ya.ru>
2023-04-21 12:27:42 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-import pkg from 'gulp'
-const { src, dest } = pkg
+import glob from 'glob'
+import path from 'path'
+import sharp from 'sharp'
 
-import changed    from 'gulp-changed'
-import responsive from 'gulp-image-resize'
-
-function compress() {
-	return src('img/src/**/*')
-	.pipe(changed('img/dist'))
-	.pipe(responsive({ width: '1920', quality: 1 }))
-	.pipe(dest('img/dist/'))
+async function compress() {
+	let files = glob.sync('img/src/**/*')
+	files.forEach(function (file) {
+		let filename = path.basename(file)
+		sharp(file)
+			.resize({ width: 1920 })
+			.withMetadata()
+			.toFile(`img/dist/${filename}`)
+			.catch((err) => { console.log(err) })
+	})
 }
 
 export {compress}
