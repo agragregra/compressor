71774e12d32bee051efdf91f2a2d408a05955782
Алексей <agragregra@ya.ru>
2025-02-09 16:19:43 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ import path from 'path'
 import sharp from 'sharp'
 
 async function compress() {
-	let files = globSync('img/src/**/*')
-	files.forEach(function (file) {
-		let filename = path.basename(file)
-		sharp(file)
-			.resize({ width: 1920 })
-			.withMetadata()
-			.toFile(`img/dist/${filename}`)
-			.catch((err) => { console.log(err) })
-	})
+  let files = globSync('img/src/**/*')
+  files.forEach(function (file) {
+    let filename = path.basename(file)
+    sharp(file)
+      .resize({ width: 1920 })
+      .withMetadata()
+      .toFile(`img/dist/${filename}`)
+      .catch((err) => { console.log(err) })
+  })
 }
 
 export {compress}
